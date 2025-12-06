7fa88c0521df63ab182ed6b3974a278c58d1f44b
TangShiMei02 <2244182951@qq.com>
2025-12-06 15:44:05 +0800

    fix: 排除大文件，限制内存占用

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    assetsInlineLimit: 0,          // 禁止内联任何资源
+    chunkSizeWarningLimit: 2000,   // 把警告阈值调到 2 MB，避免误报
+    rollupOptions: {
+      external: [                  // 让 rollup 完全不理这些大文件
+        /wallpaper\/dynamic/,
+        /wallpaper\/static/
+      ]
+    }
+  }
 })
